63b34a0f711767226fe12e60594ed1b2db3ef828
Ha Nguyen <nguyenttha89@gmail.com>
2018-07-17 21:00:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function takeANumber(katzDeliLine, customername)
+{
+  var string
+  if (katzDeliLine.length == 0)
+  {
+    katzDeliLine.push(customername)
+    string = `Welcome, ${customername}. You are number 1 in line.`
+  }
+  else
+  {
+    katzDeliLine.push(customername)
+    string = `Welcome, ${customername}. You are number ${katzDeliLine.length} in line.`
+  }
+  return string
+}
+function nowServing(katzDeliLine)
+{
+  if (katzDeliLine.length == 0)
+  {
+    return "There is nobody waiting to be served!"
+  }
+  else
+  {
+    
+  }
+}
