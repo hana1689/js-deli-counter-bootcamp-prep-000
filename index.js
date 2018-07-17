c58667d700da3895beb865d56dfbacc60c8c614c
Ha Nguyen <nguyenttha89@gmail.com>
2018-07-17 21:10:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ function nowServing(katzDeliLine)
 }
 function currentLine(line)
 {
+  var string
   if (line.length === 0)
   {
     return "The line is currently empty."
@@ -34,7 +35,8 @@ function currentLine(line)
   {
     for (let i = 0; i < line.length; i++)
     {
-      
+      string += `${i + 1}. ${line[i]}`
     }
+    return `The line is currently: ${string}`
   }
 }
