9ef2a6f2828d6a366d565f1d4bcb0ad13b17dbd6
Ha Nguyen <nguyenttha89@gmail.com>
2018-07-17 21:25:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function currentLine(line)
     {
       string += `${i + 1}. ${line[i]}, `
     }
-    string.slice(string[string.length - 1])
+    string.substr(string[string.length - 1])
     return `The line is currently: ${string}`
   }
 }
